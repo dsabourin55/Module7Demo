32e0da6a2e3f7c4d0d5a0b94c8496d23c32f1cbe
dsabourin55 <dsabourin55@rrc.ca>
2025-04-17 13:05:22 -0500

    added node dependencies and updated script

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//Deleted alert
-alert("Hello Earth!");
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+function updateDOM(value) {
+    let resultDiv = document.getElementById("result");
+    if (resultDiv) {
+        resultDiv.innerText = `The result of the operation is ${value}`;
+    }
+}
+
+function init() {
+    let result = addNumbers(10, 20);
+    updateDOM(result);
+}
+
+if (typeof window !== "undefined") {
+    window.onload = init;
+} else {
+    module.exports = { addNumbers, updateDOM, init };
+}
